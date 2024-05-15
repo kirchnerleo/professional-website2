5a8ec8ad37da5dcb99b3e1dba3fb4b34c525bd91
kirchnerleo <kirchnera2707@my.uwstout.edu>
2024-05-15 13:31:01 -0500

    added about this site info

--- professional-website/src/components/Contact.vue
+++ professional-website/src/components/Contact.vue
@@ -2,5 +2,6 @@
   <header>CONTACT</header>
   <a href="https://www.linkedin.com/in/leo-kirchner-121bb5267"> LinkedIn </a>
   <a href="https://uwstout.joinhandshake.com/stu/users/45045598"> Handshake </a>
+  <a href="https://github.com/kirchnerleo?tab=overview"> Github </a>
   kirchnera2707@my.uwstout.edu
 </template>
