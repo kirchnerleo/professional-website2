4b2f93238a626a23d3127bc0ae37aaf8cb758dc1
kirchnerleo <kirchnera2707@my.uwstout.edu>
2024-05-15 13:12:10 -0500

    filler info for home page

--- professional-website/src/components/Home.vue
+++ professional-website/src/components/Home.vue
@@ -1,3 +1,19 @@
 <template>
   <header>HOME PAGE</header>
+  <div>
+    I'm Leo Kirchner, a B.S. Computer Science - Interdisciplinary student at the
+    University of Wisconsin - Stout. I started my college education at
+    Normandale Community College in 2019, where I discovered my love for
+    programming and computers. I transferred to Stout in the fall of 2022 where
+    I will be finishing my degree. I'm always excited to learn new things, and
+    in the ever expanding field of computer science, I'm confident I'll never
+    run out of opportunities to expand my knowledge.
+  </div>
+  <div class="card flex justify-content-center">
+    <img
+      src="/src/assets/portrait_placeholder.png"
+      alt="Headshot"
+      width="250"
+    />
+  </div>
 </template>
